ff43fbbe77a2446a6589d75a4b16aebca169a7a1
SuyamaDaichi <symdit@gmail.com>
2020-03-08 14:46:56 +0900

    init

---
+++ app/src/main/java/com/symdit/swarmplus/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.symdit.swarmplus
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
